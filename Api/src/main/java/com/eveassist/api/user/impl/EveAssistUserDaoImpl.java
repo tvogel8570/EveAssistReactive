64410ae2d6cc2bdcd18e07d4369808779a8c28dc
Timothy Vogel <Timothy@vogelware.com>
2023-06-10 01:01:08 -0500

    correct refactor rename

--- Api/src/main/java/com/eveassist/api/user/impl/EveAssistUserDaoImpl.java
+++ Api/src/main/java/com/eveassist/api/user/impl/EveAssistUserDaoImpl.java
@@ -4,7 +4,7 @@ import com.eveassist.api.user.EveAssistUserCustomDao;
 import com.eveassist.api.user.entity.EveAssistUser;
 import reactor.core.publisher.Mono;
 
-public class EveAssistUserUserDaoImpl implements EveAssistUserCustomDao {
+public class EveAssistUserDaoImpl implements EveAssistUserCustomDao {
     @Override
     public Mono<EveAssistUser> findUserWithPrivileges(String userName) {
         return Mono.empty();
