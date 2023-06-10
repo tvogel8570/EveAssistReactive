fd0d834394d9a0c5c2d8191f6445195acc1358c1
Timothy Vogel <Timothy@vogelware.com>
2023-06-10 01:01:44 -0500

    add Repository stereotype

--- Api/src/main/java/com/eveassist/api/user/EveAssistUserDao.java
+++ Api/src/main/java/com/eveassist/api/user/EveAssistUserDao.java
@@ -2,8 +2,10 @@ package com.eveassist.api.user;
 
 import com.eveassist.api.user.entity.EveAssistUser;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface EveAssistUserDao extends EveAssistUserCustomDao, ReactiveCrudRepository<EveAssistUser, Long> {
     Mono<EveAssistUser> findEveAssistUserByUserUnique(String userUnique);
 }
