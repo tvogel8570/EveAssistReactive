ae12f342efbad13c89eb1121118a20fc0001cec7
Timothy Vogel <Timothy@vogelware.com>
2023-06-11 21:10:59 -0500

    working Integration test for R2DBC

--- Api/src/test/java/com/eveassist/api/user/impl/EveAssistUserDaoImplTest.java
+++ Api/src/test/java/com/eveassist/api/user/impl/EveAssistUserDaoImplTest.java
@@ -1,20 +1,48 @@
 package com.eveassist.api.user.impl;
 
+import io.r2dbc.spi.ConnectionFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.core.io.Resource;
+import org.springframework.r2dbc.connection.init.ScriptUtils;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @DataR2dbcTest
-@ActiveProfiles("test")
+@Testcontainers
 class EveAssistUserDaoImplTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> dbCont = new PostgreSQLContainer<>("postgres:15-alpine");
 
     @Autowired
     EveAssistUserDaoImpl cut;
+
+    @Autowired
+    ConnectionFactory connectionFactory;
+
+    private void executeSqlScriptBlocking(final Resource sqlScript) {
+        Mono.from(connectionFactory.create())
+                .flatMap(connection -> ScriptUtils.executeSqlScript(connection, sqlScript)
+                        .then(Mono.from(connection.close())))
+                .block();
+    }
+
+    @BeforeEach
+    void setupData(@Value("classpath:/fixture/user/user.sql") Resource sqlScript) {
+        executeSqlScriptBlocking(sqlScript);
+    }
 
     @Test
     void findUserWithPrivileges() {
@@ -28,8 +56,9 @@ class EveAssistUserDaoImplTest {
     void findAllUserWithRoles() {
         StepVerifier.create(cut.findAllUsersAndRoles())
                 .expectSubscription()
-                .expectNextMatches(eau->eau.getEmail().equals("test@test.com") && eau.getRoles().size() == 1 )
-                .expectNextMatches(eau->eau.getEmail().equals("test2@test.com") && eau.getRoles().size() == 2 )
+                .expectNextMatches(eau -> eau.getEmail().equals("test@test.com") && eau.getRoles().size() == 1)
+                .expectNextMatches(eau -> eau.getEmail().equals("test2@test.com") && eau.getRoles().size() == 2)
+                .expectNextCount(1)
                 .verifyComplete();
     }
 
@@ -37,7 +66,7 @@ class EveAssistUserDaoImplTest {
     void findUserAndRoles() {
         StepVerifier.create(cut.findUserAndRoles("123456789012345678901234567890"))
                 .expectSubscription()
-                .expectNextMatches(eau->eau.getEmail().equals("test@test.com") && eau.getRoles().size() == 1 )
+                .expectNextMatches(eau -> eau.getEmail().equals("test@test.com") && eau.getRoles().size() == 1)
                 .verifyComplete();
     }
 }
---
+++ Api/src/test/resources/fixture/user/user.sql
@@ -0,0 +1,15 @@
+delete
+from users_roles;
+delete
+from eve_assist_user;
+insert into public.eve_assist_user (id, create_date, email, eau_password, eau_user_unique, eau_screen_name)
+values (1, '2023-06-10 01:34:45.000000', 'test@test.com', 'asdf', '123456789012345678901234567890', 'test user'),
+       (2, '2023-06-10 06:55:38.000000', 'test2@test.com', 'qwer', '12345678901234567890123456789A',
+        'another test user'),
+       (3, '2023-06-10 06:55:38.000000', 'test3@test.com', 'qwer', '12345678901234567890123456789B', 'test3 user');
+
+insert into users_roles (user_id, role_id)
+VALUES (1, 0),
+       (2, 1),
+       (2, 2),
+       (3, 2);
