f3ce22e1940ac133fdc734d5b6741c9455bb55ab
Timothy Vogel <Timothy@vogelware.com>
2023-06-11 23:13:42 -0500

    rearrange and add default values for id from sequence

--- Api/src/main/resources/db/migration/V1.0__base.sql
+++ Api/src/main/resources/db/migration/V1.0__base.sql
@@ -1,8 +1,28 @@
+-- public.eve_assist_user_id_seq definition
+-- DROP SEQUENCE IF EXISTS public.eve_assist_user_id_seq;
+CREATE SEQUENCE IF NOT EXISTS public.eve_assist_user_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 100
+    CACHE 1
+    NO CYCLE;
+
+-- public.eve_pilot_id_seq definition
+-- DROP SEQUENCE IF EXISTS public.eve_pilot_id_seq;
+CREATE SEQUENCE IF NOT EXISTS public.eve_pilot_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1
+    NO CYCLE;
+
 -- public.eve_assist_user definition
 -- DROP TABLE public.eve_assist_user;
 CREATE TABLE IF NOT EXISTS public.eve_assist_user
 (
-    id          int8         NOT NULL,
+    id          int8         NOT NULL DEFAULT nextval('eve_assist_user_id_seq'),
     create_date timestamp    NOT NULL,
     email       varchar(200) NOT NULL,
     password    text         NOT NULL,
@@ -18,7 +38,7 @@ CREATE TABLE IF NOT EXISTS public.eve_assist_user
 -- DROP TABLE public.eve_pilot;
 CREATE TABLE IF NOT EXISTS public.eve_pilot
 (
-    id                 int8         NOT NULL,
+    id                 int8         NOT NULL DEFAULT nextval('eve_pilot_id_seq'),
     base_portrait_link varchar(100) NOT NULL,
     corporation_id     int8         NOT NULL,
     eve_owner_id       int8         NOT NULL,
@@ -31,23 +51,3 @@ CREATE TABLE IF NOT EXISTS public.eve_pilot
     CONSTRAINT eve_pilot_eve_assist_user_fk FOREIGN KEY (fk_eau) REFERENCES public.eve_assist_user (id),
     CONSTRAINT eve_pilot_pkey PRIMARY KEY (id)
 );
-
--- public.eve_assist_user_id_seq definition
--- DROP SEQUENCE IF EXISTS public.eve_assist_user_id_seq;
-CREATE SEQUENCE IF NOT EXISTS public.eve_assist_user_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-    CACHE 1
-    NO CYCLE;
-
--- public.eve_pilot_id_seq definition
--- DROP SEQUENCE IF EXISTS public.eve_pilot_id_seq;
-CREATE SEQUENCE IF NOT EXISTS public.eve_pilot_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-    CACHE 1
-    NO CYCLE;
